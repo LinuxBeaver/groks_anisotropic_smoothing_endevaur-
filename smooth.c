0357c9459a09e1cf3e0711745dc4cdd606f6356a
LinuxBeaver <78667207+LinuxBeaver@users.noreply.github.com>
2025-04-24 01:34:59 -0400

    Add files via upload

--- smooth.c
+++ smooth.c
@@ -24,30 +24,30 @@
 
 #ifdef GEGL_PROPERTIES
 
-property_int (iterations, _("Iterations"), 5)
+property_int (iterations, _("Iterations"), 10)
   description (_("Number of iterations; more iterations cause stronger smoothing"))
   value_range (1, 20)
-  ui_range (1, 10)
+  ui_range (1, 15)
 
-property_double (alpha, _("Alpha"), 0.2)
+property_double (alpha, _("Alpha"), 0.6)
   description (_("Diffusion strength in homogeneous regions"))
-  value_range (0.0, 0.5)
-  ui_range (0.0, 0.3)
+  value_range (0.1, 1.0)
+  ui_range (0.1, 0.8)
 
-property_double (kappa, _("Kappa"), 20.0)
+property_double (kappa, _("Kappa"), 4.0)
   description (_("Edge sensitivity parameter; lower values preserve sharper edges"))
-  value_range (5.0, 50.0)
-  ui_range (5.0, 30.0)
+  value_range (1.0, 15.0)
+  ui_range (1.0, 10.0)
 
-property_double (strength, _("Strength"), 1.0)
+property_double (strength, _("Strength"), 2.5)
   description (_("Overall intensity of the smoothing effect"))
-  value_range (0.0, 2.0)
-  ui_range (0.0, 1.5)
+  value_range (0.5, 5.0)
+  ui_range (0.5, 4.0)
 
-property_double (delta_t, _("Delta T"), 0.1)
+property_double (delta_t, _("Delta T"), 0.3)
   description (_("Time step for numerical stability"))
-  value_range (0.01, 0.2)
-  ui_range (0.01, 0.15)
+  value_range (0.05, 0.5)
+  ui_range (0.05, 0.4)
 
 #else
 
@@ -84,7 +84,7 @@ static inline gfloat
 conductance (gfloat gradient, gfloat kappa)
 {
   gfloat g = gradient / kappa;
-  return expf (-g * g);
+  return expf (-g * g); /* Gaussian conductance for edge-preserving smoothing */
 }
 
 static gboolean
@@ -97,7 +97,7 @@ process (GeglOperation       *operation,
   GeglProperties *o = GEGL_PROPERTIES (operation);
   const Babl *format = babl_format ("RGBA float");
   GeglBufferIterator *iter;
-  gfloat *temp_buf;
+  GeglBuffer *temp;
   gint i;
 
   if (result->width < 2 || result->height < 2)
@@ -107,146 +107,127 @@ process (GeglOperation       *operation,
       return TRUE;
     }
 
-  /* Allocate temporary buffer for one iteration */
-  temp_buf = g_new (gfloat, result->width * result->height * 4);
-  if (!temp_buf)
-    {
-      g_warning ("Failed to allocate temporary buffer");
-      return FALSE;
-    }
-
-  /* Initialize iterator for input and output */
-  iter = gegl_buffer_iterator_new (input, result, 0, format,
-                                  GEGL_ACCESS_READ, GEGL_ABYSS_CLAMP, 2);
-  gegl_buffer_iterator_add (iter, output, result, 0, format,
-                           GEGL_ACCESS_WRITE, GEGL_ABYSS_CLAMP);
-
-  /* Copy input to output for first iteration */
-  while (gegl_buffer_iterator_next (iter))
-    {
-      gfloat *in_data = iter->items[0].data;
-      gfloat *out_data = iter->items[1].data;
-      memcpy (out_data, in_data, iter->length * 4 * sizeof (gfloat));
-    }
+  /* Create a temporary buffer for intermediate results */
+  temp = gegl_buffer_new (result, format);
+  gegl_buffer_copy (input, result, GEGL_ABYSS_CLAMP, temp, result);
 
   /* Perform diffusion iterations */
   for (i = 0; i < o->iterations; i++)
     {
-      iter = gegl_buffer_iterator_new (output, result, 0, format,
-                                      GEGL_ACCESS_READ, GEGL_ABYSS_CLAMP, 2);
+      iter = gegl_buffer_iterator_new (temp, result, 0, format,
+                                      GEGL_ACCESS_READ, GEGL_ABYSS_LOOP, 2);
       gegl_buffer_iterator_add (iter, output, result, 0, format,
                                GEGL_ACCESS_WRITE, GEGL_ABYSS_CLAMP);
 
       while (gegl_buffer_iterator_next (iter))
         {
-          gfloat *src = iter->items[0].data;
-          gfloat *dst = iter->items[1].data;
+          gfloat *in_data = iter->items[0].data;
+          gfloat *out_data = iter->items[1].data;
           GeglRectangle roi = iter->items[0].roi;
-          gint x, y;
+          GeglRectangle in_roi = roi;
+          /* Expand ROI by 2 pixels for gradient calculations */
+          in_roi.x -= 2;
+          in_roi.y -= 2;
+          in_roi.width += 4;
+          in_roi.height += 4;
+          gegl_rectangle_intersect (&in_roi, &in_roi, result);
 
-          /* Copy to temporary buffer */
-          memcpy (temp_buf, src, iter->length * 4 * sizeof (gfloat));
-
-          for (y = 0; y < roi.height; y++)
+          /* Create a temporary buffer for the expanded ROI */
+          gfloat *in_expanded = g_new0 (gfloat, in_roi.width * in_roi.height * 4);
+          GeglBufferIterator *in_iter = gegl_buffer_iterator_new (temp, &in_roi, 0, format,
+                                                                 GEGL_ACCESS_READ, GEGL_ABYSS_LOOP, 1);
+          while (gegl_buffer_iterator_next (in_iter))
             {
-              for (x = 0; x < roi.width; x++)
+              gfloat *data = in_iter->items[0].data;
+              GeglRectangle r = in_iter->items[0].roi;
+              gint x, y;
+              for (y = 0; y < r.height; y++)
+                for (x = 0; x < r.width; x++)
+                  {
+                    gint src_idx = (y * r.width + x) * 4;
+                    gint dst_idx = ((y + r.y - in_roi.y) * in_roi.width + (x + r.x - in_roi.x)) * 4;
+                    memcpy (in_expanded + dst_idx, data + src_idx, 4 * sizeof (gfloat));
+                  }
+            }
+
+          for (gint y = 0; y < roi.height; y++)
+            {
+              for (gint x = 0; x < roi.width; x++)
                 {
                   gint offset = (y * roi.width + x) * 4;
+                  gint in_x = x + roi.x - in_roi.x;
+                  gint in_y = y + roi.y - in_roi.y;
+                  gint in_offset = (in_y * in_roi.width + in_x) * 4;
                   gfloat sum[4] = {0.0f, 0.0f, 0.0f, 0.0f};
-                  gfloat weights[8] = {0.0f};
-                  gfloat gradients[8][4];
+                  gfloat weights[4] = {0.0f};
+                  gfloat gradients[4][4] = {{0.0f}};
                   gint j;
 
-                  /* Compute gradients in eight directions */
-                  if (x > 0)
+                  /* Compute gradients in four directions (N, S, E, W) */
+                  if (in_x > 0) /* West */
                     {
-                      gint idx = (y * roi.width + (x - 1)) * 4;
+                      gint idx = (in_y * in_roi.width + (in_x - 1)) * 4;
                       for (j = 0; j < 4; j++)
-                        gradients[0][j] = temp_buf[idx + j] - temp_buf[offset + j];
-                      weights[0] = conductance (gradients[0][0], o->kappa);
+                        gradients[0][j] = in_expanded[idx + j] - in_expanded[in_offset + j];
+                      weights[0] = conductance (fabsf (gradients[0][0]), o->kappa);
                     }
 
-                  if (x < roi.width - 1)
+                  if (in_x < in_roi.width - 1) /* East */
                     {
-                      gint idx = (y * roi.width + (x + 1)) * 4;
+                      gint idx = (in_y * in_roi.width + (in_x + 1)) * 4;
                       for (j = 0; j < 4; j++)
-                        gradients[1][j] = temp_buf[idx + j] - temp_buf[offset + j];
-                      weights[1] = conductance (gradients[1][0], o->kappa);
+                        gradients[1][j] = in_expanded[idx + j] - in_expanded[in_offset + j];
+                      weights[1] = conductance (fabsf (gradients[1][0]), o->kappa);
                     }
 
-                  if (y > 0)
+                  if (in_y > 0) /* North */
                     {
-                      gint idx = ((y - 1) * roi.width + x) * 4;
+                      gint idx = ((in_y - 1) * in_roi.width + in_x) * 4;
                       for (j = 0; j < 4; j++)
-                        gradients[2][j] = temp_buf[idx + j] - temp_buf[offset + j];
-                      weights[2] = conductance (gradients[2][0], o->kappa);
+                        gradients[2][j] = in_expanded[idx + j] - in_expanded[in_offset + j];
+                      weights[2] = conductance (fabsf (gradients[2][0]), o->kappa);
                     }
 
-                  if (y < roi.height - 1)
+                  if (in_y < in_roi.height - 1) /* South */
                     {
-                      gint idx = ((y + 1) * roi.width + x) * 4;
+                      gint idx = ((in_y + 1) * in_roi.width + in_x) * 4;
                       for (j = 0; j < 4; j++)
-                        gradients[3][j] = temp_buf[idx + j] - temp_buf[offset + j];
-                      weights[3] = conductance (gradients[3][0], o->kappa);
-                    }
-
-                  if (x > 0 && y > 0)
-                    {
-                      gint idx = ((y - 1) * roi.width + (x - 1)) * 4;
-                      for (j = 0; j < 4; j++)
-                        gradients[4][j] = temp_buf[idx + j] - temp_buf[offset + j];
-                      weights[4] = conductance (gradients[4][0], o->kappa) * 0.707f;
-                    }
-
-                  if (x < roi.width - 1 && y > 0)
-                    {
-                      gint idx = ((y - 1) * roi.width + (x + 1)) * 4;
-                      for (j = 0; j < 4; j++)
-                        gradients[5][j] = temp_buf[idx + j] - temp_buf[offset + j];
-                      weights[5] = conductance (gradients[5][0], o->kappa) * 0.707f;
-                    }
-
-                  if (x > 0 && y < roi.height - 1)
-                    {
-                      gint idx = ((y + 1) * roi.width + (x - 1)) * 4;
-                      for (j = 0; j < 4; j++)
-                        gradients[6][j] = temp_buf[idx + j] - temp_buf[offset + j];
-                      weights[6] = conductance (gradients[6][0], o->kappa) * 0.707f;
-                    }
-
-                  if (x < roi.width - 1 && y < roi.height - 1)
-                    {
-                      gint idx = ((y + 1) * roi.width + (x + 1)) * 4;
-                      for (j = 0; j < 4; j++)
-                        gradients[7][j] = temp_buf[idx + j] - temp_buf[offset + j];
-                      weights[7] = conductance (gradients[7][0], o->kappa) * 0.707f;
+                        gradients[3][j] = in_expanded[idx + j] - in_expanded[in_offset + j];
+                      weights[3] = conductance (fabsf (gradients[3][0]), o->kappa);
                     }
 
                   /* Update pixel values */
-                  for (j = 0; j < 8; j++)
+                  gfloat weight_sum = weights[0] + weights[1] + weights[2] + weights[3];
+                  if (weight_sum > 1e-6f)
                     {
-                      if (weights[j] > 0.0f)
+                      gfloat w = o->alpha * o->strength / weight_sum;
+                      for (j = 0; j < 4; j++)
                         {
-                          gfloat w = weights[j] * o->alpha * o->strength;
-                          sum[0] += w * gradients[j][0];
-                          sum[1] += w * gradients[j][1];
-                          sum[2] += w * gradients[j][2];
-                          sum[3] += w * gradients[j][3];
+                          sum[j] = w * (weights[0] * gradients[0][j] +
+                                        weights[1] * gradients[1][j] +
+                                        weights[2] * gradients[2][j] +
+                                        weights[3] * gradients[3][j]);
+                          sum[j] = CLAMP (sum[j], -2.0f, 2.0f); /* Wider clamp for stronger effect */
                         }
                     }
 
                   for (j = 0; j < 4; j++)
                     {
-                      gfloat value = temp_buf[offset + j] + o->delta_t * sum[j];
-                      /* Clamp to prevent NaN or extreme values */
-                      dst[offset + j] = CLAMP (value, 0.0f, 1.0f);
+                      gfloat value = in_data[offset + j] + o->delta_t * sum[j];
+                      out_data[offset + j] = CLAMP (value, 0.0f, 1.0f);
                     }
                 }
             }
+
+          g_free (in_expanded);
         }
+
+      /* Copy output to temp for next iteration */
+      gegl_buffer_copy (output, result, GEGL_ABYSS_CLAMP, temp, result);
     }
 
-  g_free (temp_buf);
+  /* Clean up */
+  g_object_unref (temp);
   return TRUE;
 }
 
